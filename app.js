8d1c8ac77955d9b157e36bd69fc90417b0963beb
mjtrembley <m.trembley96@gmail.com>
2023-01-12 21:54:31 -0500

    Updated app.js - fix on line 16. Updated public/index.html to reference valid files

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { hasSubscribers } = require('diagnostics_channel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 //register handlebars partials 
