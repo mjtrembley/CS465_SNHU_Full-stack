99483e0abd9b4a45bb5d91413a7841d89820e866
mjtrembley <m.trembley96@gmail.com>
2023-01-31 19:56:10 -0500

    Finished Module 5

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,6 +6,8 @@ const tripsController = require('../controllers/trips');
 router
     .route('/trips')
     .get(tripsController.tripsList)
+router 
+    .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode);
 
 module.exports = router;
